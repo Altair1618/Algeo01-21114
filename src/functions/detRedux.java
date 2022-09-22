028c41747723fc2d67ed9b44291e05f68b166feb
natthankrish <92136335+natthankrish@users.noreply.github.com>
2022-09-22 22:50:10 +0700

    First commit

---
+++ src/functions/detRedux.java
@@ -0,0 +1,58 @@
+public class detRedux {
+    Matrix onMatrix;
+    final double UNDEF_DET = -9999.99;
+
+    public detRedux(Matrix inMatrix) {
+        this.onMatrix = inMatrix;
+    }
+
+    public double detByReduction () {
+        double det = 1;
+        if (!onMatrix.isSquare()) {
+            return UNDEF_DET;
+        }
+        // Matriks berbentuk persegi
+        if (onMatrix.countElement() == 1) {
+            return onMatrix.getElement(0, 0);
+        } 
+
+        // Matriks bukan berukuran 1 x 1
+        for (int i = 0; i < onMatrix.column - 1; i++) {
+            boolean isValid = true;
+            if (onMatrix.getElement(i+1, i+1) == 0) {
+                isValid = false;
+                for (int search_index = i+1; search_index < onMatrix.row; search_index++) {
+                    if (onMatrix.getElement(search_index+1, i+1) != 0) {
+                        onMatrix.swapRow(i+1, search_index+1);
+                        isValid = true;
+                        break;
+                    }
+                }
+            }
+            if (!isValid) {
+                return 0;
+            }
+            for (int j = i+1; j < onMatrix.row; j++) {
+                
+                double zero = onMatrix.makeZero(onMatrix.getElement(j+1, i+1), onMatrix.getElement(i+1, i+1));
+                
+                onMatrix.OBE(i, j, zero);
+            }
+            
+            // Determinan
+            for (int j = 0; j < onMatrix.row; j++){
+                det *= onMatrix.getElement(j+1, j+1);
+            }
+        }
+        return det;
+    }
+
+    public static void main (String[] args) {
+        Matrix m = new Matrix();
+        m.readMatrix();
+        double determinant;
+        detRedux det = new detRedux(m);
+        determinant = det.detByReduction();
+        System.out.print(determinant);
+    }
+}
