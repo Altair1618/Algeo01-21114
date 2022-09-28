6a55ba58f4bf70b8dd7f1fb04085de4647deb72d
Altair1618 <farhannabilsuryono3@gmail.com>
2022-09-28 21:56:21 +0700

    Nambahin bilinear

--- src/functions/Bicubic.java
+++ src/functions/Bicubic.java
@@ -44,13 +44,73 @@ public class Bicubic {
         return Operations.multiplyMatrix(invX, m);
     }
 
-    public static double calcBicubic(Matrix cof, double x, double y) {
+    public static double calcBicubic(Matrix m, double x, double y) {
+        Matrix invX = generateInverseMatrixX();
+        Matrix mcof = getBicubicCof(invX, convMatrix(m));
+
         double val = 0;
 
         int idx = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                val += cof.getElement(idx + 1, 1) * Math.pow(x, i) * Math.pow(y, j);
+                val += mcof.getElement(idx + 1, 1) * Math.pow(x, i) * Math.pow(y, j);
+                idx++;
+            }
+        }
+
+        return val;
+    }
+
+    public static Matrix generateInverseMatrixXBL() {
+        Matrix m = new Matrix();
+        m.createMatrix(4, 4);
+
+        int row = 0;
+        for (int x = 0; x <= 1; x++) {
+            for (int y = 0; y <= 1; y++) {
+                int col = 0;
+                for (int i = 0; i <= 1; i++) {
+                    for (int j = 0; j <= 1; j++) {
+                        m.setElement(Math.pow(x, i) * Math.pow(y, j), row + 1, col + 1);
+                        col++;
+                    }
+                }
+                row++;
+            }
+        }
+
+        return Gauss.inverseGauss(m);
+    }
+
+    public static Matrix convMatrixBL(Matrix m) {
+        Matrix res = new Matrix();
+        res.createMatrix(4, 1);
+
+        int row = 0;
+        for (int i = 0; i < m.getRowLength(); i++) {
+            for (int j = 0; j < m.getColumnLength(); j++) {
+                res.setElement(m.getElement(i + 1, j + 1), row + 1, 1);
+                row++;
+            }
+        }
+
+        return res;
+    }
+
+    public static Matrix getBilinearCof(Matrix invX, Matrix m) {
+        return Operations.multiplyMatrix(invX, m);
+    }
+
+    public static double calcBilinear(Matrix m, double x, double y) {
+        Matrix invX = generateInverseMatrixXBL();
+        Matrix mcof = getBilinearCof(invX, convMatrixBL(m));
+
+        double val = 0;
+
+        int idx = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                val += mcof.getElement(idx + 1, 1) * Math.pow(x, i) * Math.pow(y, j);
                 idx++;
             }
         }
@@ -59,12 +119,11 @@ public class Bicubic {
     }
 
     public static void main(String[] args) {
-        Matrix invX = generateInverseMatrixX();
+        // Matrix invX = generateInverseMatrixX();
 
         Matrix m = new Matrix();
         m.readMatrix();
 
-        m = getBicubicCof(invX, convMatrix(m));
-        System.out.println(calcBicubic(m, 0.5, 0.5));
+        System.out.println(calcBilinear(m, 0.5, 0.5));
     }
 }
---
+++ src/functions/ScaleImage.java
@@ -0,0 +1,68 @@
+package functions;
+
+import dataStructure.Matrix;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class ScaleImage {
+    public static Matrix addPadding(Matrix m) {
+        Matrix pad = new Matrix();
+        pad.createMatrix(m.getRowLength() + 3, m.getColumnLength() + 3);
+
+        // Matriks Awal
+        for (int i = 0; i < m.getRowLength(); i++) {
+            for (int j = 0; j < m.getColumnLength(); j++) {
+                pad.setElement(m.getElement(i + 1, j + 1), i + 2, j + 2);
+            }
+        }
+
+        // Padding Atas
+        for (int i = 0; i < pad.getColumnLength(); i++) {
+            if (i == 0) {
+                pad.setElement(m.getElement(1, 1), 1, 1);
+            } else if (i >= pad.getColumnLength() - 2) {
+                pad.setElement(m.getElement(1, m.getColumnLength()), 1, i + 1);
+            } else {
+                pad.setElement(m.getElement(1, i), 1, i + 1);
+            }
+        }
+
+        // Padding Kiri
+        for (int i = 0; i < m.getRowLength(); i++) {
+            pad.setElement(m.getElement(i + 1, 1), i + 2, 1);
+        }
+
+        // Padding Kanan
+        for (int i = 0; i < m.getRowLength(); i++) {
+            pad.setElement(m.getElement(i + 1, m.getColumnLength()), i + 2, pad.getColumnLength() - 1);
+            pad.setElement(m.getElement(i + 1, m.getColumnLength()), i + 2, pad.getColumnLength());
+        }
+
+        // Padding Bawah
+        for (int i = 0; i < pad.getColumnLength(); i++) {
+            if (i == 0) {
+                pad.setElement(m.getElement(m.getRowLength(), 1), pad.getRowLength() - 1, 1);
+                pad.setElement(m.getElement(m.getRowLength(), 1), pad.getRowLength(), 1);
+            } else if (i >= pad.getColumnLength() - 2) {
+                pad.setElement(m.getElement(m.getRowLength(), m.getColumnLength()), pad.getRowLength() - 1, i + 1);
+                pad.setElement(m.getElement(m.getRowLength(), m.getColumnLength()), pad.getRowLength(), i + 1);
+            } else {
+                pad.setElement(m.getElement(m.getRowLength(), i), pad.getRowLength() - 1, i + 1);
+                pad.setElement(m.getElement(m.getRowLength(), i), pad.getRowLength(), i + 1);
+            }
+        }
+
+        return pad;
+    }
+
+    public static void main(String[] args) {
+        Matrix m = new Matrix();
+        m.readMatrix();
+
+        Matrix pad = addPadding(m);
+        pad.writeMatrix();
+    }
+}
