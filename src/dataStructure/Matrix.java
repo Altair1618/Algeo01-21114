3f65840c062c92dc470b117f47b0fb3d6b68a261
natthankrish <92136335+natthankrish@users.noreply.github.com>
2022-09-28 20:50:05 +0700

    add readMatrixPoint()

--- src/dataStructure/Matrix.java
+++ src/dataStructure/Matrix.java
@@ -124,6 +124,97 @@ public class Matrix {
         }
     }
 
+    public void readMatrixPoint () {
+        Scanner choiceInput = new Scanner(System.in);
+        System.out.println("Pilih cara input Point: ");
+        System.out.println("- Melalui Keyboard (Ketik 1) ");
+        System.out.println("- Melalui File (Ketik 2)");
+        System.out.print("Pilihan: ");
+        int choice = choiceInput.nextInt();
+
+        while (choice != 1 && choice != 2) {
+            System.out.println("Input salah! Masukkan input yang benar");
+            readMatrixPoint();
+        }
+
+        if (choice == 1) {
+            readMatrixPointFromKeyboard();       
+        } else {
+            readMatrixPointFromFile();
+        }
+        choiceInput.close();
+    }
+
+    public void readMatrixPointFromKeyboard () {
+        Scanner input = new Scanner(System.in);
+        int count;
+
+        System.out.print("Masukkan jumlah titik: ");
+        count = input.nextInt();
+
+        this.matrix = new double[count][2];
+        this.row = count;
+
+        for (int i = 0; i < this.row; i++) {
+            System.out.print("Input titik ke ");
+            System.out.println(i+1);
+            Point inputitem = new Point();
+            inputitem.readPoint();
+            this.matrix[i][0] = inputitem.x;
+            this.matrix[i][1] = inputitem.y;
+        }
+        //input.close();
+    }
+
+    public void readMatrixPointFromFile () {
+        String filePath;
+
+        System.out.print("Masukkan file path: ");
+        Scanner inputFilePath = new Scanner(System.in);
+        filePath = inputFilePath.nextLine();
+        
+        try {
+            File matrixFile = new File(filePath);
+            Scanner readMatrixFile = new Scanner(matrixFile);
+            ArrayList<double[]> inputMatrix = new ArrayList<double[]>();
+                
+            while (readMatrixFile.hasNextLine()) {
+                String line = readMatrixFile.nextLine();
+                double[] inputRow = new double[2];
+                int j = 0;
+                for (int i = 0; i < line.length(); i++) {
+                    String currentString = "";
+                    while ((line.charAt(i) != ' ')) {
+                        currentString += line.charAt(i);
+                        i++;
+                        if (i == line.length()) {
+                            break;
+                        }
+                    } // (line.charAt(i) == ' ') 
+                    inputRow[j] = Double.parseDouble(currentString);
+                    j++;
+                }
+                inputMatrix.add(inputRow);
+            }
+
+
+            this.row = inputMatrix.size();
+            this.column = 2;
+
+            this.matrix = new double[this.row][2];
+        
+            for (int i = 0; i < this.row; i++) {
+                this.matrix[i][0] = inputMatrix.get(i)[0];
+                this.matrix[i][1] = inputMatrix.get(i)[1];
+            }
+
+            inputMatrix.clear();
+        } catch (FileNotFoundException e) {
+            System.out.println("File tidak ditemukan");
+        }
+    }
+
+
     //FUNGSI PRIMITIF SELEKTOR
     public double getElement (int numRow, int numCol) {
         // Mengambil elemen pada matriks
@@ -178,6 +269,15 @@ public class Matrix {
         // Mengubah nilai dari matriks pada baris numRow dan kolom numCol, numRow dan numCol adalah INDEX 
         this.matrix[numRow - 1][numCol - 1] = val;
     }
+
+    public double[][] getMatrixPoint () {
+        return this.matrix;
+    }
+
+    public int getCountPoint () {
+        return this.row;
+    }
+
 
     //FUNGSI PRIMITIF LAINNYA
     public void writeMatrix () {
@@ -378,6 +478,9 @@ public class Matrix {
             }
         }
     }
-
-
 }
+
+
+
+  
+ 
