8d1ae54941c3524cc5e3495f8377d84fb2a072b3
natthankrish <92136335+natthankrish@users.noreply.github.com>
2022-09-29 23:21:35 +0700

    change function to static

--- src/functions/detRedux.java
+++ src/functions/detRedux.java
@@ -2,14 +2,10 @@ package functions;
 import dataStructure.*;
 
 public class detRedux {
-    Matrix onMatrix;
-    final double UNDEF_DET = -9999.99;
-
-    public detRedux(Matrix inMatrix) {
-        this.onMatrix = inMatrix;
-    }
-
-    public double detByReduction () {
+    public static double detByReduction (Matrix inMatrix) {
+        Matrix onMatrix;
+        final double UNDEF_DET = -9999.99;
+        onMatrix = inMatrix;
         double det = 1;
         if (!onMatrix.isSquare()) {
             return UNDEF_DET;
@@ -41,21 +37,12 @@ public class detRedux {
                 
                 onMatrix.OBE(i, j, zero);
             }
-            
-            // Determinan
-            for (int j = 0; j < onMatrix.getRowLength(); j++){
-                det *= onMatrix.getElement(j+1, j+1);
-            }
         }
-        return det;
-    }
 
-    public static void main (String[] args) {
-        Matrix m = new Matrix();
-        m.readMatrix();
-        double determinant;
-        detRedux det = new detRedux(m);
-        determinant = det.detByReduction();
-        System.out.print(determinant);
+        for (int j = 0; j < onMatrix.getRowLength(); j++){
+            det *= onMatrix.getElement(j+1, j+1);
+        }
+
+        return det;
     }
 }
