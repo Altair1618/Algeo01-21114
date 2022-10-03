654da7afb3499c712ff3f3f06368f85ba759ed78
natthankrish <92136335+natthankrish@users.noreply.github.com>
2022-10-03 19:37:29 +0700

    Minor changes

--- src/functions/Interpolasi_polinom.java
+++ src/functions/Interpolasi_polinom.java
@@ -1,6 +1,9 @@
 package functions;
 import dataStructure.*;
 import utility.Operations;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
@@ -34,13 +37,11 @@ public class Interpolasi_polinom {
 		solution.createMatrix(n, 1);
 		solution = Operations.multiplyMatrix(temp, B);
 		solution.roundMatElmt(5);
-		solution.writeMatrix();
 		return solution;
 	}
 	
 	public static void displayPolinom(Matrix solution) {
 		int n = solution.getRowLength();
-		System.out.println("Fungsi polinomial: ");
 		System.out.printf("f(x) = ");
 		if (solution.getElement(1, 1) != 0) {
 			System.out.print(solution.getElement(1, 1) + " ");
@@ -65,6 +66,37 @@ public class Interpolasi_polinom {
 		}
 		System.out.println();
 	}
+
+	public static void writePolinom(FileWriter fileWriter, Matrix solution) {
+		try {
+			int n = solution.getRowLength();
+			fileWriter.write("f(x) = ");
+			if (solution.getElement(1, 1) != 0) {
+				fileWriter.write(String.valueOf(solution.getElement(1, 1) + " "));
+			}
+			if (solution.getElement(2, 1) > 0) {
+				fileWriter.write(String.valueOf("+ (" + solution.getElement(2, 1) + ")x "));
+			}
+			else if (solution.getElement(2, 1) < 0) {
+				fileWriter.write(String.valueOf("- (" + (-solution.getElement(2, 1)) + ")x "));
+			}
+			
+			for (int i=3; i<=n; i++) {
+				if (i % 5 ==0) {
+					fileWriter.write("\n");
+				}
+				if (solution.getElement(i, 1) > 0) {
+					fileWriter.write(String.valueOf("+ (" + solution.getElement(i, 1) + ")x^" + (i-1) + " "));
+				}
+				else if (solution.getElement(i, 1) < 0) {
+					fileWriter.write(String.valueOf("- (" + (-solution.getElement(i, 1)) + ")x^" + (i-1) + " "));
+				}
+			}
+			fileWriter.write("\n");
+		} catch (IOException e) {
+			System.out.println("Terjadi Kesalahan");
+		}
+	}
 	
 	public static void displayValueOfFx(Matrix solution, Matrix findValueOf) {
 		double result;
@@ -81,17 +113,27 @@ public class Interpolasi_polinom {
 			
 		}
 	}
-		
-		
-	
-	public static void main(String[] args) {
-		Matrix diket = new Matrix();
-		diket.readMatrix();
-		Matrix cari = new Matrix();
-		cari.readMatrix();
-		
-		displayPolinom(interpolasi(diket));
-		displayValueOfFx(interpolasi(diket), cari);
+
+	public static void writeValueOfFx(FileWriter fileWriter, Matrix solution, Matrix findValueOf) {
+		try {
+			double result;
+			double x;
+			int n = findValueOf.getCountPoint();
+			for (int i=1; i<=n; i++) {
+				result =0;
+				x=findValueOf.getElement(i, 1);
+				for (int j=1; j<=solution.getRowLength(); j++) {
+					result += solution.getElement(j, 1)*Math.pow(x, j-1);
+				}
+				result = BigDecimal.valueOf(result).setScale(5, RoundingMode.HALF_UP).doubleValue();
+				fileWriter.write("f(");
+				fileWriter.write(String.valueOf(x));
+				fileWriter.write(") = ");
+				fileWriter.write(String.valueOf(result));
+				fileWriter.write("\n");
+			}
+		} catch (IOException e) {
+			System.out.println("Terjadi Kesalahan");
+		}
 	}
-	
 }
